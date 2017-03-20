f5dd79f752ca1c382fcc18fd53e570c893e1e04f
Theodoros Christofis <theoc1970@hotmail.com>
2017-03-20 07:56:23 +0200

    Add project files.

---
+++ AppMaker/gulpfile.js
@@ -0,0 +1,7 @@
+﻿/// <binding BeforeBuild='compile-sass' />
+var gulp = require('gulp');
+var gulpSass = require('gulp-sass');
+gulp.task('compile-sass', function () {
+    gulp.src('./wwwroot/lib/bootstrap/scss/bootstrap.scss')
+        .pipe(gulpSass()).pipe(gulp.dest('./wwwroot/css'));
+});
