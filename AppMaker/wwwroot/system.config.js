4c118cb922eaac2d91774bd9b35e66487d7a749c
Theodoros Christofis <theoc1970@hotmail.com>
2017-05-07 11:24:50 +0300

    fix

--- AppMaker/wwwroot/system.config.js
+++ AppMaker/wwwroot/system.config.js
@@ -30,8 +30,7 @@
             // other libraries
             'rxjs': 'npm:rxjs',
             'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
-            'ngx-toastr': 'npm:ngx-toastr/toastr.umd.js',
-            'AdminLTE': 'lib/admin-lte/dist/js/app.js'
+            'ngx-toastr': 'npm:ngx-toastr/toastr.umd.js'
         },
         // packages tells the System loader how to load when no filename and/or no extension
         packages: {
